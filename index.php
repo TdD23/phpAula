6b1f4ed4d505ef856d21160b6b56108e30972bf5
TdD23 <troopsd6@gmail.com>
2025-05-31 12:55:21 -0300

    ok

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 24/05/2025</title>
+</head>
+<body>
+
+<?php
+
+function aguarde () {
+    echo "Aguarde um momento, por favor! </br>";
+}
+/*
+aguarde();
+aguarde();
+aguarde();
+aguarde();
+aguarde();
+aguarde();
+
+
+function CAT($largura, $comprimento) {
+    $area = $largura * $comprimento;
+    echo '</br>';
+    return $area;
+
+}
+
+echo CAT(30, 50);
+echo CAT(100, 84);*/
+
+
+$lista_fruta = array ('banana', 'maçã', 'morango', 'uva');
+//print_r($lista_fruta);
+var_dump($lista_fruta);
+echo $lista_fruta [2];
+
+
+?>
+    
+</body>
+</html>
